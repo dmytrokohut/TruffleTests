10e835a61823b85d5f975886321e4262a1f9674a
Dima Kohut <retr0finr1r@gmail.com>
2017-12-26 16:18:52 +0200

    The test to testrpc network has been rewrited

--- test/TestBallotTestrpc.js
+++ test/TestBallotTestrpc.js
@@ -1,121 +1,94 @@
 var Ballot = artifacts.require("Ballot");
 
 contract('Ballot (testrpc network)', accounts => {
-  it("should be deployed", () => {
-    return Ballot.deployed().then(ballot => {
-      let address = ballot.address;
-      assert.isDefined(address, "The contract is not deployed");
-    });
+  it("should be deployed", async () => {
+    let ballot = await Ballot.deployed();
+    assert.isDefined(ballot, "The contract is not deployed");
   });
 
-  it("should have correct chairperson address", () => {
-    return Ballot.deployed().then(ballot => {
-      return ballot.chairperson.call();
-    }).then(chairperson => {
-      assert.equal(chairperson, accounts[0], "The chairperson address is not correct");
-    });
+  it("should have correct chairperson address", async () => {
+    let ballot = await Ballot.deployed();
+    let chairperson = await ballot.chairperson.call();
+    assert.equal(chairperson, accounts[0], "The chainperson address is not correct");
   });
 
-  it("should have 3 correct candidate names", () => {
-    let ballot;
-    let first_name;
-    let second_name;
-    let third_name;
+  it("should have correct candidate names", async () => {
+    let ballot = await Ballot.deployed();
+    let firstName = await ballot.proposals.call(0);
+    let secondName = await ballot.proposals.call(1);
+    let thirdName = await ballot.proposals.call(2);
 
-    return Ballot.deployed().then(instance => {
-        ballot = instance;
-        return ballot.proposals.call(0);
-    }).then(proposal => {
-      first_name = web3.toUtf8(proposal[0]);
-      return ballot.proposals.call(1);
-    }).then(proposal => {
-      second_name = web3.toUtf8(proposal[0]);
-      return ballot.proposals.call(2);
-    }).then(proposal => {
-      third_name = web3.toUtf8(proposal[0]);
-
-      assert.equal(first_name, "Name 1", "Incorrect first candidate name in list");
-      assert.equal(second_name, "Name 2", "Incorrect second candidate name in list");
-      assert.equal(third_name, "Name 3", "Incorrect third candidate name in list");
-    });
+    assert.equal(web3.toUtf8(firstName[0]), "Name 1", "Incorrect first candidate name");
+    assert.equal(web3.toUtf8(secondName[0]), "Name 2", "Incorrect second candidate name");
+    assert.equal(web3.toUtf8(thirdName[0]), "Name 3", "Incorrect third candidate name");
   });
 
-  it("should give rights to vote", () => {
-    let ballot;
+  it("should give rights to vote", async () => {
+    let ballot = await Ballot.deployed();
 
-    return Ballot.deployed().then(instance => {
-      ballot = instance;
-      return ballot.giveRightToVote(accounts[1]);
-    }).then(() => {
-      return ballot.voters.call(accounts[1]);
-    }).then(voter => {
-      assert.equal(voter[0].toNumber(), 1, "The voter don't have the vote right");
-    });
+    // this must return error: "invalid opcode" such as accounts[1] don't have permissions.
+    await ballot.giveRightToVote(accounts[0], {from: accounts[1]}).then(() => {
+      console.log("\tWrong behavior of contract, \"accounts[1]\" should not have permissions for this operation such as it is not a chainperson");
+    }).catch(exception => { });
+
+    // this must give permission for voting.
+    await ballot.giveRightToVote(accounts[1], {from: accounts[0]});
+    let voter = await ballot.voters.call(accounts[1]);
+
+    // this must return error: "invalid opcode" such as accounts[1] already has permission for voting.
+    await ballot.giveRightToVote(accounts[1], {from: accounts[0]}).then(() => {
+      console.log("\tWrong behavior of contract, accounts[1] should already has permission for voting");
+    }).catch(exception => { });
+
+    assert.equal(voter[0].toNumber(), 1, "The voter didn't get the permission to vote");
   });
 
-  it("should correct execute delegation", () => {
-    let ballot;
+  it("should vote for candidate", async () => {
+    let ballot = await Ballot.deployed();
+    let numberOfVotesBefore = (await ballot.proposals.call(0))[1].toNumber();
+    await ballot.vote(0, {from: accounts[1]});
 
-    return Ballot.deployed().then(instance => {
-      ballot = instance;
-      return ballot.delegate(accounts[0], {from: accounts[1]});
-    }).then(() => {
-      return ballot.voters.call(accounts[1]);
-    }).then(voter => {
-      let isVoted = voter[1];
-      let delegateTo = voter[2];
+    let numberOfVotesAfter = (await ballot.proposals.call(0))[1].toNumber();
+    let voter = await ballot.voters.call(accounts[1]);
 
-      assert.isTrue(isVoted, "The delegation was not succeed");
-      assert.equal(delegateTo, accounts[0], "The address of delegate is not correct");
-    });
+    // this must return exception such as "accounts[1]" has already voted
+    await ballot.vote(0, {from: accounts[1]}).then(() => {
+      console.log("\tWrong behavior of contract, \"accounts[1]\" has already voted");
+    }).catch(exception => { });
+
+    assert.equal(numberOfVotesAfter - numberOfVotesBefore, voter[0].toNumber(), "Incorrect number of weight");
+    assert.isTrue(voter[1], "The \"voted\" field has not changed");
+    assert.equal(voter[3].toNumber(), 0, "Incorrect number of proposal");
   });
 
-  it("should vote for candidate", () => {
-    let ballot;
-    let numberOfVotesBefore, numberOfVotesAfter;
-    let votedFor;
-    let isVoted;
-    let weight;
+  it("should execute delegation", async () => {
+    let ballot = await Ballot.deployed();
 
-    return Ballot.deployed().then(instance => {
-      ballot = instance;
-      return ballot.proposals.call(0);
-    }).then(proposal => {
-      numberOfVotesBefore = proposal[1].toNumber();
-      return ballot.vote(0);
-    }).then(() => {
-      return ballot.proposals.call(0);
-    }).then(proposal => {
-      numberOfVotesAfter = proposal[1].toNumber();
-      return ballot.voters.call(accounts[0]);
-    }).then(voter => {
-      weight = voter[0];
-      isVoted = voter[1];
-      votedFor = voter[3];
-      let difference = numberOfVotesAfter - numberOfVotesBefore;
+    // this must return exception such as "accounts[1]" has already voted
+    await ballot.delegate(accounts[0], {from: accounts[1]}).then(() => {
+      console.log("\tWrong behavior of contract, \"accounts[1]\" has already voted so it can't delegete voted another person");
+    }).catch(exception => { });
 
-      assert.isTrue(isVoted, "The voter was not voting");
-      assert.equal(votedFor, 0, "The proposal's number is different");
-      assert.equal(difference, weight, "The number of votes has not changed");
-    });
+    // this must return exception such as account cannot delegate to itself
+    await ballot.delegate(accounts[0], {from: accounts[0]}).then(() => {
+      console.log("\tWrong behavior, account can't delegate to itself");
+    }).catch(exception => { });
+
+    // this must delegate properly
+    await ballot.delegate(accounts[1], {from: accounts[0]});
+    let voter = await ballot.voters.call(accounts[0]);
+
+    assert.isTrue(voter[1], "After delegation \"voted\" field must become \"true\"");
+    assert.equal(voter[2], accounts[1], "Incorrect address in field \"delegate\"");
   });
 
-  it("should display correct winner canidate", () => {
-    let ballot;
-    let winnerNumber;
+  it("should display the proper winner", async () => {
+    let ballot = await Ballot.deployed();
+    let winnerProposalNumber = await ballot.winningProposal.call();
+    let winnerProposalName = await ballot.winnerName.call();
 
-    return Ballot.deployed().then(instance => {
-      ballot = instance;
-      return ballot.winningProposal.call();
-    }).then(winnerProposal => {
-      winnerNumber = winnerProposal;
-      return ballot.winnerName.call();
-    }).then(proposal => {
-      let winner = web3.toUtf8(proposal);
-
-      assert.equal(winnerNumber, 0, "Incorrect winner's candidate number");
-      assert.equal(winner, "Name 1", "Incorrect winner's name");
-    });
+    assert.equal(winnerProposalNumber, 0, "Incorrect winner's number");
+    assert.equal(web3.toUtf8(winnerProposalName), "Name 1", "Incorrect winner's name");
   });
 
 });
